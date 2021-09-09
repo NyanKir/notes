1614eb4484d511734874bccaba016886d2657826
Kirill Sytnyanskiy <nyan.kirill@gmail.com>
2021-09-09 14:31:00 +0300

    inital files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <ButtonCreate/>
+            <section className={styles.section}>
+                <h1>Notes</h1>
+            </section>
+        </div>
+    );
+}
+
+function ButtonCreate() {
+    return (
+        <div className={styles.wrapper}>
+            <button className="btn button_dark button_plus"></button>
+                <div className={`${styles.bubble} button_primary`}/>
+                <div className={`${styles.bubble} button_danger`}/>
+                <div className={`${styles.bubble} button_warning`}/>
+                <div className={`${styles.bubble} button_purple`}/>
+                <div className={`${styles.bubble} button_accept`}/>
+        </div>
+    )
 }
 
 export default App;
